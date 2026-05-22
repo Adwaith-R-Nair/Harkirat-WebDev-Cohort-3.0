7cf2fd04ef378261f03117d9b796a5eb66446fdf
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-05-22 11:39:37 +0530

    greet update

--- Week-01/W-1.2/user.js
+++ Week-01/W-1.2/user.js
@@ -1,13 +1,16 @@
 function greet(user){
     console.log("Hello " + user.name);
     console.log("You are " + user.age + " years old.");
+    console.log("You are a " + user.gender);
     console.log("Hello " + user["name"]);
     console.log("You are " + user["age"] + " years old.");
+    console.log("You are a " + user["gender"]);
 }
 
 let user = {
     name : "Adwaith",
-    age : "22"
+    age : "22",
+    gender : "Male"
 };
 
 greet(user);
