99704664674cf3223e95a1717216fdf4c46e9bbb
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-05-23 14:27:04 +0530

    b.txt fs

--- Week-02/W-2.1/fs.js
+++ Week-02/W-2.1/fs.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname, "a.txt");
+const filePath2 = path.join(__dirname, "b.txt");
 
 const contents = fs.readFileSync(filePath, "utf-8");
 console.log(contents);
+
+const contents2 = fs.readFileSync(filePath2, "utf-8");
+console.log(contents2);
