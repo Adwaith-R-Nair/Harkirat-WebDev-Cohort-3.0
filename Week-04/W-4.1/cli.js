731784fc9df00c3a36efd2faa3de55ef7419d863
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-07-12 15:22:42 +0530

    cli.js

---
+++ Week-04/W-4.1/cli.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+    .name('counter')
+    .description('CLI to do file based tasks')
+    .version('0.8.0');
+
+program.command('count')
+    .description('Count the number of words in a file')
+    .argument('<file>', 'file to count')
+    .action((file) => {
+        fs.readFile(file, "utf-8", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const words = data.split(' ').length;
+                const lines = data.split('\n').length;
+                console.log(`There are ${words} words in ${file}`);
+                console.log(`There are ${lines} lines in ${file}`);
+            }
+        });
+    });
+
+program.parse();
