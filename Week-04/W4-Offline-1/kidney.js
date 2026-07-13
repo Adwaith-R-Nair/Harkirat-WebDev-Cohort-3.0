5869e0027850b036264c73b5766f5b71d879c8a1
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-07-13 16:30:20 +0530

    number of kidneys

---
+++ Week-04/W4-Offline-1/kidney.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }, {
+        healthy: true
+    }]
+}];
+
+app.get("/", function(req, res) {
+    const JohnKidneys = users[0].kidneys;
+    const numberofkidneys = JohnKidneys.length;
+    let healthykidneys = 0;
+    for(let i = 0; i < numberofkidneys; i++) {
+        if(JohnKidneys[i].healthy) {
+            healthykidneys++;
+        }
+    }
+    const unhealthykidneys = numberofkidneys - healthykidneys;
+
+    res.json({
+        numberofkidneys,
+        healthykidneys,
+        unhealthykidneys
+    });
+});
+
+app.listen(3000);
